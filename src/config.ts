f7723c08c768db3aa2c1550c679c4c82be015583
Ricky Li <i@liqi.link>
2025-03-09 16:51:42 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -51,8 +51,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  enable: false,
-  avatar: '',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Ricky Li',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
