5f28c5c2b4fd15c4af9427e54e383567342cb49d
Ricky Li <i@liqi.link>
2025-03-11 17:38:09 +0800

    use giscus

--- src/config.ts
+++ src/config.ts
@@ -56,22 +56,22 @@ export const profileConfig: ProfileConfig = {
   name: 'Ricky Li',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
+    // {
+    //   name: 'Twitter',
+    //   icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+    //                                     // You will need to install the corresponding icon set if it's not already included
+    //                                     // `pnpm add @iconify-json/<icon-set-name>`
+    //   url: 'https://twitter.com',
+    // },
+    // {
+    //   name: 'Steam',
+    //   icon: 'fa6-brands:steam',
+    //   url: 'https://store.steampowered.com',
+    // },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/liqimore',
     },
   ],
 }
@@ -83,23 +83,23 @@ export const licenseConfig: LicenseConfig = {
 }
 
 export const commentConfig: CommentConfig = {
-  disqus: {
-    shortname: 'fuwari',
-  },
-  // giscus: {
-  //   repo: 'moeyua/fuwari',
-  //   repoId: 'R_kgDOKy9HOQ',
-  //   category: 'General',
-  //   categoryId: 'DIC_kwDOKy9HOc4CegmW',
-  //   mapping: 'title',
-  //   strict: '0',
-  //   reactionsEnabled: '1',
-  //   emitMetadata: '1',
-  //   inputPosition: 'top',
-  //   theme: 'light',
-  //   lang: 'zh-CN',
-  //   loading: 'lazy',
+  // disqus: {
+  //   shortname: 'fuwari',
   // },
+  giscus: {
+    repo: 'liqimore/giscus-comment',
+    repoId: 'R_kgDOOGroXg',
+    category: 'Announcements',
+    categoryId: 'DIC_kwDOOGroXs4Cn0ux',
+    mapping: 'og:title',
+    strict: '0',
+    reactionsEnabled: '1',
+    emitMetadata: '1',
+    inputPosition: 'top',
+    theme: 'light',
+    lang: 'zh-CN',
+    loading: 'lazy',
+  },
   // twikoo: {
   //   envId: 'https://twikoo-tau-flame.vercel.app',
   // },
